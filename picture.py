90e625aee5c9253e936c015c4b4ad9693f7690d5
Y.Sasaki <y-sasaki@intworks.co.jp>
2019-02-21 01:14:51 +0900

    [add]画像生成処理

---
+++ picture.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+# coding: utf-8
+# coding=utf-8
+# -*- coding: utf-8 -*-
+# vim: fileencoding=utf-8
+
+import sys
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFilter
+
+img = Image.open("white.jpg")
+img.filter(ImageFilter.GaussianBlur(4.0))
+draw = ImageDraw.Draw(img)
+
+img_width = 1920
+img_height = 1278
+
+
+#######これと
+out_text_size = (img_width + 1, img_height + 1)
+font_size_offset = 0
+players_fontFile = 'KouzanMouhituFontOTF.otf'
+players_font = ImageFont.truetype(players_fontFile, 300, encoding='utf-8')
+players = '羽生善治九段 - 豊島将之二冠（王位・棋聖)'
+# フォントの描画サイズが描画領域のサイズを下回るまでwhile
+while img_width < out_text_size[0] + 300 or img_height < out_text_size[1]+ 300:
+    players_font = ImageFont.truetype(players_fontFile, 150 - font_size_offset)
+    # draw.textsizeで描画時のサイズを取得
+    out_text_size = draw.textsize(players, font=players_font)
+    font_size_offset += 1
+players_w, players_h = players_font.getsize(players)
+players_x = (img_width - players_w)/2
+players_y = (img_height - players_h)/2
+draw.text((players_x, players_y - 350), players, fill=("#000000"), font=players_font)
+
+#######これが一緒の為メソッドに切り出す
+out_text_size = (img_width + 1, img_height + 1)
+font_size_offset = 0
+title_fontFile = 'KouzanMouhituFontOTF.otf'
+title_font = ImageFont.truetype(title_fontFile, 80, encoding='utf-8')
+title = 'ヒューリック杯棋聖戦 二次予選'
+# フォントの描画サイズが描画領域のサイズを下回るまでwhile
+while img_width < out_text_size[0]  + 300 or img_height < out_text_size[1] + 300:
+    title_font = ImageFont.truetype(title_fontFile, 80 - font_size_offset)
+    # draw.textsizeで描画時のサイズを取得
+    out_text_size = draw.textsize(title, font=title_font)
+    font_size_offset += 1
+title_w, title_h = title_font.getsize(title)
+title_x = (img_width - title_w)/2
+title_y = (img_height - title_h)/2
+draw.text((title_x, title_y + 420), title, fill=("#000000"), font=title_font)
+
+
+# im.show() #即、プレビューする時はここのコメントアウトを外す
+img.save('resize_img.jpg', 'JPEG', quality=100, optimize=True)
