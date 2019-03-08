635de9449f5cece6e75383b1b1ddf6cd3d6bef31
Y.Sasaki <y-sasaki@intworks.co.jp>
2019-03-08 12:11:12 +0900

    [upd]mainファイル名変更

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import boto3
+from base64 import b64decode
 from bs4 import BeautifulSoup
 import requests
 import re
 import json
 import sys
 from copy import deepcopy
-from time import time
 from time import sleep
 import datetime
 import urllib.parse
+from enum import Enum
 
 import picture
 import deleteMessage
 
-from enum import Enum
 
 class titleCount(Enum):
     一 = 1
@@ -27,8 +29,8 @@ class titleCount(Enum):
     八 = 8
 
 ladies_title = ["女流王座", "女流名人", "女流王位", "女流王将", "倉敷藤花", "清麗", "女王"]
-
-h = {"User-Agent":"#"}
+special_lady = ["中井広恵", "西山朋佳", "加藤桃子", "中七海"]
+h = {"User-Agent": boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['user_agent']))['Plaintext']}
 
 tommorow_flg = False
 row_num = 1
@@ -41,17 +43,15 @@ all_row_list = []
 def getPlayerInfo(tag, row_list):
     
     global ladies_count
-
-    if "中井広恵" in str(tag.text) or "西山朋佳" in str(tag.text) :
+    
+    if len([s for s in special_lady if s in tag.text]):
         ladies_count += 1
 
     if "※" in tag.text:
-        # LPSA所属棋士に付けられる「※」を除いてリストに追加
         row_list.append(tag.text.strip(" ""※"))
         row_list.append("(LPSA)")
         ladies_count += 1
     elif "＊" in tag.text: 
-        # フリー棋士に付けられる「＊」を除いてリストに追加
         row_list.append(tag.text.strip(" ""＊"))
         row_list.append("(フリー)")
     elif "アマ" in tag.text: 
@@ -59,23 +59,26 @@ def getPlayerInfo(tag, row_list):
         row_list.append("(アマ)")
     else:
         link = tag.find("a", href=re.compile("^/player/"))
-        player_page = requests.get('https://www.shogi.or.jp' + link["href"], headers=h)
-        sleep(1)
-        player_soup = BeautifulSoup(player_page.content, "lxml") 
-        row_list.append(tag.text.strip(" "))
-        # 段位・称号をリストに追加
-        status = player_soup.select(".headingElementsA01.min.ico03")[0].text.strip(" ")
-        if "・" in status:
-            status = str(titleCount(status.count("・") + 1).name) + "冠"
+        if link is not None:
+            player_page = requests.get('https://www.shogi.or.jp' + link["href"], headers=h)
+            sleep(1)
+            player_soup = BeautifulSoup(player_page.content, "lxml") 
+            row_list.append(tag.text.strip(" "))
+            status = player_soup.select(".headingElementsA01.min.ico03")[0].text.strip(" ")
 
-        if status in ladies_title:
-            ladies_count += 1
-        elif status.count("女流"):
-            status = status.replace("女流", "") 
-            ladies_count += 1
-        elif status.count("引退"):
-            status = "九段"
-        row_list.append(status)
+            if status in ladies_title:
+                ladies_count += 1
+            elif status.count("女流"):
+                ladies_count += 1
+            elif status.count("引退"):
+                status = "九段"
+                    
+            if "・" in status:
+                status = str(titleCount(status.count("・") + 1).name) + "冠"
+            row_list.append(status)         
+        else:
+            row_list.append(tag.text.strip(" "))
+            row_list.append("")
 
 def postSlackMessage(row_list, image_url):
 
@@ -107,33 +110,31 @@ def postSlackMessage(row_list, image_url):
     else:
         text = '*' + str(tomorrow_str) + ' - 第' + str(row_num) + '組*'
         fallback = str(tomorrow_str) + " " + row_list[2] + row_list[3]  + " - " + row_list[4] + row_list[5]
-    # Slackに取得した情報を送信する
     payload = {
         "attachments": [
             {
                 "fallback": fallback,
-                # "color": "#2eb886",
 		        "text": text,
                 "image_url": image_url,
                 "actions": [
                     {
                         "type": "button",
                         "name": "player1",
-                        "text": ":mag: " + row_list[2],
+                        "text": ":bust_in_silhouette: " + row_list[2],
                         "url": player1,
                         "style": "normal",
                     },
                     {
                         "type": "button",
                         "name": "player2",
-                        "text": ":mag: " + row_list[4],
+                        "text": ":bust_in_silhouette: " + row_list[4],
                         "url": player2,
                         "style": "normal",
                     },
                     {
                         "type": "button",
                         "name": "title",
-                        "text": ":mag: " + row_list[0],
+                        "text": ":newspaper: " +  row_list[0] ,
                         "url": 'https://www.shogi.or.jp' + row_list[1],
                         "style": "normal",
                     }
@@ -142,7 +143,7 @@ def postSlackMessage(row_list, image_url):
         ]
     }
 
-    requests.post('#', data=json.dumps(payload))
+    requests.post(boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['incoming_web_hook_url']))['Plaintext'], data=json.dumps(payload))
 
 def irregularPostSlackMessage():
     tomorrow_date = datetime.date.today() + datetime.timedelta(days=1)
@@ -150,18 +151,14 @@ def irregularPostSlackMessage():
     payload = {
         "text": str(tomorrow_str) + "は対局予定がありません。"
     }
-
-    requests.post('#', data=json.dumps(payload))
+    requests.post(boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['incoming_web_hook_url']))['Plaintext'], data=json.dumps(payload))
 
 def main():
     deleteMessage.exec()
-    # 週間対局予定ページを開く
     r = requests.get('https://www.shogi.or.jp/game/#jsTabE01_02', headers=h)
-    soup = BeautifulSoup(r.content, "lxml") # r.textだと文字化けする
-    tommorow_day =  str(datetime.datetime.now().day + 1) # 明日の日にちを取得
-
+    soup = BeautifulSoup(r.content, "lxml")
+    tommorow_day = str((datetime.datetime.now() + datetime.timedelta(days = 1)).day)
     for tag in soup.find(id="jsTabE01_02").find_all("td"):
-        # 週間対局予定ページの日にち箇所の判定
         if re.search('colspan="5"', str(tag)) is not None:
             global tommorow_flg
             if re.search("月" + tommorow_day + "日|月" + tommorow_day +"・|・" + tommorow_day +"日", str(tag)):
@@ -175,7 +172,7 @@ def main():
             global row_num
             global col_num
             global ladies_count
-            # 棋戦名が他の行とグルーピングされていて存在しない場合、上の行から棋戦名をコピーする
+            global row_list
             if col_num == 1 and re.search('class="tac"', str(tag)): 
                 row_list.append(all_row_list[row_num - 2][0])
                 row_list.append(all_row_list[row_num - 2][1])
@@ -187,7 +184,6 @@ def main():
                 row_list.append(link["href"])
             elif col_num == 2 or col_num == 3:
                 getPlayerInfo(tag, row_list)
-            # 列番号をリセットする
             elif col_num == 5:
                 row_list.append(tag.text.strip(" "))
                 all_row_list.append(deepcopy(row_list))
@@ -203,5 +199,6 @@ def main():
             col_num += 1
     if row_num == 1:
         irregularPostSlackMessage()
+
 if __name__ == '__main__':
     main()
